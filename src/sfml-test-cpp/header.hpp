ea39580972759ce386419ecc331fa8d7dcef142f
Gemuele Aludino <g.aludino@gmail.com>
2021-07-10 16:35:15 -0400

    Add sfml-test-cpp

---
+++ src/sfml-test-cpp/header.hpp
@@ -0,0 +1,22 @@
+/*!
+    \file       header.hpp
+    \brief      Header file
+
+    \author
+    \date
+ */
+
+#ifndef HEADER_HPP
+#define HEADER_HPP
+
+#include <iostream>
+#include <vector>
+#include <memory>
+#include <algorithm>
+#include <functional>
+#include <exception>
+
+int helloWindow();
+
+#endif /* HEADER_HPP */
+
