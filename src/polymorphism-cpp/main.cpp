c044ac285611b3df47be576babee536c46c36ff9
Gemuele Aludino <g.aludino@gmail.com>
2021-07-10 21:37:00 -0400

    Clear polymorphism-cpp

--- src/polymorphism-cpp/main.cpp
+++ src/polymorphism-cpp/main.cpp
@@ -17,16 +17,6 @@
     \return     0 on success, non-zero on failure
  */
 int main(int argc, const char *argv[]) {
-    auto rect = rect2(0, 0, 3, 7);
-
-    std::cout << &rect << " has an area of " << rect.area() << std::endl;
-
-    rect.draw();
-
-    auto eqtri = eqtri2(0, 0, 3);
-    std::cout << &eqtri << " has an area of " << eqtri.area() << std::endl;
-    
-    eqtri.draw();
 
     return EXIT_SUCCESS;
 }
