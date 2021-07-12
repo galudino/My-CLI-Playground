67b8150390f4c83d9f846271b70ab37b8348c5a4
Gemuele Aludino <g.aludino@gmail.com>
2021-07-11 20:39:48 -0400

    Add threads-c

---
+++ src/threads-c/main.c
@@ -0,0 +1,23 @@
+/*!
+    \file       main.c
+    \brief      Client source file
+
+    \author     
+    \date
+ */
+
+#include "header.h"
+
+/*!
+    \brief      Program execution begins here.
+
+    \param[in]  argc    Command line argument count
+    \param[in]  argv    Command line arguments 
+
+    \return     0 on success, non-zero on failure
+ */
+int main(int argc, const char *argv[]) {
+    puts("Hello, world!");
+    return EXIT_SUCCESS;
+}
+
